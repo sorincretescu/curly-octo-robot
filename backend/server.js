787288ebf40b4b477b5bd31efc8455c117906fc1
andregeorgi <andreaciorban@yahoo.com>
2023-07-24 12:08:23 +0300

    changes to connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 // Import required packages
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
 // Create an instance of Express
 const app = express();
 app.use(cors());
 
 // MongoDB connection URI
-const uri = "mongodb+srv://aleneagu96:vBjfPY8qNECMNwcI@todo.orth1vn.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:albastrugri@todos.nfgv3pq.mongodb.net/";
 
 // Create a MongoDB client
 const client = new MongoClient(uri, {
@@ -17,33 +17,33 @@ const client = new MongoClient(uri, {
 });
 
 async function getDataFromDatabase() {
-    try {
-        await client.connect();
-        const database = client.db("todo");
-        const collection = database.collection("todos");
+  try {
+    await client.connect();
+    const database = client.db("Todos");
+    const collection = database.collection("Todos");
 
-        const data = await collection.find({}).toArray();
+    const data = await collection.find({}).toArray();
 
-        await client.close();
+    await client.close();
 
-        return data;
-    } catch (error) {
-        console.log('Error fetching data from the database',error);
-        throw error;
-    }
+    return data;
+  } catch (error) {
+    console.log("Error fetching data from the database", error);
+    throw error;
+  }
 }
 // Start the server
 const PORT = 5000; // You can choose any available port number
 
-app.get('/api/todos', async (req, res) => {
-    try{
-        const data = await getDataFromDatabase();
-        console.log(data);
-        res.json(data);
-    } catch (error) {
-        console.error('Error handling GET request', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+app.get("/api/todos", async (req, res) => {
+  try {
+    const data = await getDataFromDatabase();
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error("Error handling GET request", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
