52b94f9c78e9756e466e600f36c1599a57d57fe5
aleneagu96 <aleneagu96@gmail.com>
2023-07-27 17:13:37 +0300

    refactor- move logic to logic folder and add db name in .env

---
+++ backend/logic/todoLogic.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const todoSchema = require("../../database/schemas/todoSchema");
+const Todo = mongoose.model("Todo", todoSchema);
+
+async function getDataFromDatabase() {
+  try {
+    const data = await Todo.find({});
+    console.log("Data retrieved:", data);
+    return data ?? [];
+  } catch (error) {
+    console.log("Error fetching data from the database", error);
+    throw error;
+  }
+}
+
+async function addDataToDatabase() {
+  try {
+    const newTodo = new Todo({
+      priority: 1,
+      description: "mare titlu de testat brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr ra papa bum bum mare test",
+      subtasks: ["abcd"],
+    });
+
+    await newTodo.save();
+    console.log("Data added successfully:", newTodo);
+  } catch (error) {
+    console.error("Error adding data to the database:", error);
+    throw error;
+  }
+}
+
+module.exports = { getDataFromDatabase, addDataToDatabase };
--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const todoSchema = require("../database/schemas/todoSchema");
-const Todo = mongoose.model("Todo", todoSchema);
+const {getDataFromDatabase, addDataToDatabase} = require('./logic/todoLogic');
 
-
-// Create an instance of Express
 const app = express();
 app.use(cors());
 
-// MongoDB connection URI
 const uri = process.env.URI ?? "";
 
-// Create a MongoDB client
 const mongooseOptions = {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
@@ -28,54 +23,18 @@ const mongooseOptions = {
       console.error('Mongoose connection error:', err);
     });
     
-    // Use the 'disconnected' event to handle disconnections
     mongoose.connection.on('disconnected', () => {
       console.log('Mongoose disconnected');
     });
-
-    startServer(Todo);
-    
 
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
 })();
 
-
-async function getDataFromDatabase(Todo) {
-  try {
-    const data = await Todo.find({});
-    console.log("Data retrieved:", data); // Add this line to log the data
-    return data ?? [];
-  } catch (error) {
-    console.log("Error fetching data from the database", error);
-    throw error;
-  }
-}
-
-// Function to add data to the database
-async function addDataToDatabase() {
-  try {
-    // Create a new Todo document
-    const newTodo = new Todo({
-      priority: 1,
-      description: "mare titlu de testat brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr ra papa ",
-      subtasks:["abcd"],
-    });
-
-    // Save the document to the database
-    await newTodo.save();
-
-    console.log("Data added successfully:", newTodo);
-  } catch (error) {
-    console.error("Error adding data to the database:", error);
-  }
-}
-
-function startServer(Todo) {
   app.get("/api/todos", async (req, res) => {
     try {
-      const data = await getDataFromDatabase(Todo);
+      const data = await getDataFromDatabase();
       console.log(data);
       res.json(data);
     } catch (error) {
@@ -84,11 +43,18 @@ function startServer(Todo) {
     }
   });
 
-  app.get("/api/testing", async (req, res) => await addDataToDatabase())
+  app.get("/api/testing", async (req, res) => {
+    try {
+      await addDataToDatabase();
+      res.json({ message: "Data added successfully" });
+    } catch (error) {
+      console.error("Error handling GET request", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
 
-  // Start the server
   const PORT = 5000;
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
-}
+
